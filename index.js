3ba145c16a9d5840c60d12f0a7a598ba8cd40d7e
Raghavendra <raghavendra@housejoy.in>
2016-07-04 12:24:02 +0530

    port change

--- index.js
+++ index.js
@@ -104,6 +104,6 @@ for (var i = 0; i < config.boardSize; i++) {
     }
 }
 
-http.listen(80, function () {
-    console.log('listening on *:80');
+http.listen(3000, function () {
+    console.log('listening on *:3000');
 });
